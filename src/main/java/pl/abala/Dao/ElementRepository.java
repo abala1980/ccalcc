718765c3784f479098af7caef11c66f891f3b4db
AdB <adrian.bala@gmail.com>
2017-04-24 00:47:56 +0200

    Initial commit

---
+++ src/main/java/pl/abala/Dao/ElementRepository.java
@@ -0,0 +1,55 @@
+package pl.abala.Dao;
+
+import org.springframework.stereotype.Repository;
+import pl.abala.Entity.Element;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by AdB on 23.04.2017.
+ */
+@Repository
+public class ElementRepository implements ElementDao {
+    private static Map<Integer, Element> periodicTable;
+
+    static {
+        periodicTable = new HashMap<Integer, Element>() {
+            {
+                put(1, new Element(1, 1.00794, "H", "Hydrogen"));
+                put(2, new Element(2, 4.0026, "He", "Helium"));
+                put(3, new Element(3, 6.941, "Li", "Lithium"));
+            }
+        };
+    }
+
+    @Override
+    public Collection<Element> getPeriodicTable() {
+        return this.periodicTable.values();
+    }
+
+    @Override
+    public Element getElementByAtomicNumber(int atomicNumber) {
+        return this.periodicTable.get(atomicNumber);
+    }
+
+    @Override
+    public void delElementByAtomicNumber(int atomicNumber) {
+        this.periodicTable.remove(atomicNumber);
+    }
+
+    @Override
+    public void updElement(Element element) {
+        Element e = this.periodicTable.get(element.getAtomicNumber());
+        e.setAtomicMass(element.getAtomicMass());
+        e.setSymbol(element.getSymbol());
+        e.setName(element.getName());
+    }
+
+    @Override
+    public void addElement(Element element) {
+        this.periodicTable.put(element.getAtomicNumber(), element);
+    }
+}
